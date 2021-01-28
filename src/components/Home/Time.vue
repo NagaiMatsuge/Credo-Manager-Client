4f3b97ba337ff8aed858f0777b71ac2853c9518f
NagaiMatsuge <komilovboburweb@gmail.com>
2021-01-28 18:59:09 +0500

    Main Page Left Section is in progress

---
+++ src/components/Home/Time.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="time-wrapper">
+    <div class="time-wrapper__date">
+      Среда, 2 Сентября
+    </div>
+    <div class="time-wrapper__time">
+      10:16
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss">
+@import "@/assets/scss/components/time";
+</style>
---
+++ src/views/pages/home/Home.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="home">
+    <div class="home__app">
+      <Time />
+      <Notifications />
+      <Notes />
+    </div>
+    <div class="home__users">
+      <Users />
+    </div>
+    <div class="home__projects">
+      <Projects />
+    </div>
+  </div>
+</template>
+
+<script>
+import Time from "@/components/Home/Time";
+import Notifications from "@/components/Home/Notifications";
+import Notes from "@/components/Home/Notes";
+import Projects from "@/components/Home/Projects";
+import Users from "@/components/Home/Users";
+export default {
+  data() {
+    return {};
+  },
+  components: {
+    Time,
+    Notifications,
+    Notes,
+    Projects,
+    Users,
+  },
+};
+</script>
+
+<style lang="scss">
+@import "@/assets/scss/pages/home/app";
+</style>
